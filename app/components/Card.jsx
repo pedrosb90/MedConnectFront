a32711d57f1bd58c47c6b602aadfc818ce3151ef
maritrini <mariatrinidadsalazarr@gmail.com>
2023-05-11 23:27:52 -0500

    cambios en ruta detail registro especialidades y registro medicos

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -2,22 +2,17 @@ import Link from "next/link";
 
 export default function Card({ name, image, description, index }) {
   return (
-
-    
-    <div  key={index}>
-
-        {/* GRID */}
+    <div key={index}>
+      {/* GRID */}
       <div className="flex flex-wrap -m-4">
-
         {/* CARD */}
-          <div className="p-4 sm:w-1/2 lg:w-1/3">
-           
-            <div className="h-full border-2 border-gray-800 border-opacity-60 rounded-lg overflow-hidden ">
-              <img
-                className="lg:h-72 md:h-48 w-full object-cover object-center"
-                src={image}
-                alt=""
-                />
+        <div className="p-4 sm:w-1/2 lg:w-1/3">
+          <div className="h-full border-2 border-gray-800 border-opacity-60 rounded-lg overflow-hidden ">
+            <img
+              className="lg:h-72 md:h-48 w-full object-cover object-center"
+              src={image}
+              alt=""
+            />
             <div>
               <h1>{name}</h1>
               <h3>{description}</h3>
@@ -25,13 +20,10 @@ export default function Card({ name, image, description, index }) {
                 <button>Conocer mas</button>
               </Link>
             </div>
-            </div>
-
-                
-          
+          </div>
         </div>
       </div>
-        {/* END:GRID */}
+      {/* END:GRID */}
     </div>
-  )
+  );
 }
