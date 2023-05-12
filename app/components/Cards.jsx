310afe051fadfc5f6f1745b9cec1b98b8e3535d3
emilianoprioli <emilianoprioli.ep@gmail.com>
2023-05-12 10:55:31 -0300

    fixing card

--- app/components/Cards.jsx
+++ app/components/Cards.jsx
@@ -4,7 +4,7 @@ export default function Cards({ especialidad }) {
   
     return (
         <div className=" max-w-[1320px] mx-auto grid lg:grid-cols-4 md:grid-cols-2 gap-6 px-[20px] container">
-            {especialidad.map(({ name, image, description }, index) => {
+            {especialidad.map(({ name, url, description }, index) => {
                 return <Card2 
                 key={index}
                 name={name} 
