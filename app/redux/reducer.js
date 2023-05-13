cdcfb6175cd1d05939b6070e040745b908891d29
maritrini <mariatrinidadsalazarr@gmail.com>
2023-05-12 21:36:59 -0500

    cambios en ruta detail

--- app/redux/reducer.js
+++ app/redux/reducer.js
@@ -1,36 +1,27 @@
-'use client';
+"use client";
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState={
-    AllSpecial:[],
+const initialState = {
+  AllSpecial: [],
 
-    Datail: []
-
-}
+  Detail: {},
+};
 export const medicalReducer = createSlice({
-    name:'speciality',
-    initialState,
-    reducers:{
-        getSpeciality:(state, action)=>{
-            const data= action.payload;
-            state.AllSpecial= [...data];
+  name: "speciality",
+  initialState,
+  reducers: {
+    getSpeciality: (state, action) => {
+      const data = action.payload;
+      state.AllSpecial = [...data];
+    },
 
-            
-        },
+    getId: (state, action) => {
+      const data = action.payload;
+      state.Detail = data;
+    },
+  },
+});
 
-        getId : (state, action) => {
-            const data = action.payload;
-            state.Datail = [...data]
+export const { getSpeciality, getId } = medicalReducer.actions;
 
-        }
-    }
-
-})
-
-
-
-
-export const {getSpeciality, getId} = medicalReducer.actions
-
-export default medicalReducer.reducer
-
+export default medicalReducer.reducer;
