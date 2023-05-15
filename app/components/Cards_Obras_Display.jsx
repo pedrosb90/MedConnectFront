cd33013015cb31d05509f65db70bef3dba1e01dd
drope <pstrauch@gmail.com>
2023-05-15 16:21:57 -0300

    medicos/ rename cardMedic - Menu

--- app/components/Cards_Obras_Display.jsx
+++ app/components/Cards_Obras_Display.jsx
@@ -1,10 +1,10 @@
-import CardObras from "./CardObras";
+import Card_Obras from "./Card_Obras";
 
-export default function CardsObras({ obras }) {
+export default function Cards_Obras_Display({ obras }) {
   return (
     <div className=" font-sans max-w-[1320px] mx-auto grid lg:grid-cols-4 md:grid-cols-2 gap-6 px-[20px] container">
       {obras.map(({ img }, index) => {
-        return <CardObras key={index} img={img} />;
+        return <Card_Obras key={index} img={img} />;
       })}
     </div>
   );
