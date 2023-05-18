d8c5ba902aac47aa9173dbffa1203fcd64a82e92
drope <pstrauch@gmail.com>
2023-05-18 08:52:53 -0300

    specs

--- app/components/Search_Bar_Especialidades.jsx
+++ app/components/Search_Bar_Especialidades.jsx
@@ -1,32 +1,32 @@
 import { useState } from "react";
-import {searchBar} from "../redux/reducer"
-import axios from "axios"
+import { searchBar } from "../redux/reducer";
+import axios from "axios";
 import { useDispatch } from "react-redux";
 
 export default function SearchBar() {
+  const [name, setName] = useState("");
+  const dispatch = useDispatch();
 
-    const [name, setName] = useState("");
-    const dispatch = useDispatch();
+  const handleSearch = (event) => {
+    event.preventDefault();
+    setName(event.target.value);
+  };
 
-    const handleSearch = (event) => {
-        event.preventDefault();
-        setName(event.target.value)
-      };
-
-      async function fetchData(e) {
-        try {
-          e.preventDefault();
-          const response = await axios.get(`http://localhost:3001/specializations?name=${name}`);
-          dispatch(searchBar(response.data));
-        } catch (error) {
-          alert(error.message);
-        }
-      }
-    
+  async function fetchData(e) {
+    try {
+      e.preventDefault();
+      const response = await axios.get(
+        `http://localhost:3001/specializations?name=${name}`
+      );
+      dispatch(searchBar(response.data));
+    } catch (error) {
+      alert(error.message);
+    }
+  }
 
   return (
     <div className="flex justify-center">
-        <div className="flex gap-1">
+      <div className="flex gap-1">
         <input
           type="text"
           placeholder="Enter a specialty"
@@ -34,9 +34,14 @@ export default function SearchBar() {
           value={name}
           className="p-2 mr-1 border border-black bg-[#dedcdc] mb-2 text-base rounded-md "
         />
-        <button className="text-[rgba(255, 255, 255, 0.62)] mb-2 border border-black bg-[#dedcdc] rounded-md px-4 py-2 cursor-pointer" onClick={(e) => fetchData(e)}>SEARCH</button>
-        <button className="text-[rgba(255, 255, 255, 0.62)] mb-2 border border-black bg-[#dedcdc] rounded-md px-4 py-2 cursor-pointer"
-          
+        <button
+          className="text-[rgba(255, 255, 255, 0.62)] mb-2 border border-black bg-[#dedcdc] rounded-md px-4 py-2 cursor-pointer"
+          onClick={(e) => fetchData(e)}
+        >
+          SEARCH
+        </button>
+        <button
+          className="text-[rgba(255, 255, 255, 0.62)] mb-2 border border-black bg-[#dedcdc] rounded-md px-4 py-2 cursor-pointer"
           onClick={() => {
             window.location.reload();
           }}
@@ -45,5 +50,5 @@ export default function SearchBar() {
         </button>
       </div>
     </div>
-  )
+  );
 }
