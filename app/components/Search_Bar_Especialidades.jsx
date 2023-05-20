eb16e3a6db94fb6ea50d7b374084af3318f4c6f6
drope <pstrauch@gmail.com>
2023-05-20 16:51:16 -0300

    search especialidades styles

--- app/components/Search_Bar_Especialidades.jsx
+++ app/components/Search_Bar_Especialidades.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { searchBar } from "../redux/reducer";
+import { searchBar, clearSearch, getSpeciality } from "../redux/reducer";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 
@@ -12,48 +12,65 @@ export default function SearchBar() {
   const [searchPerformed, setSearchPerformed] = useState(false);
   const dispatch = useDispatch();
 
-  const handleSearch = (event) => {
+  const handleSearch = async (event) => {
     event.preventDefault();
-    setName(event.target.value);
-  };
-  async function fetchData(e) {
     try {
-      e.preventDefault();
       const response = await axios.get(`${local}?name=${name}`);
       dispatch(searchBar(response.data));
+      setSearchPerformed(true);
     } catch (error) {
       alert(error.message);
     }
-  }
+  };
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleReset = () => {
+    setName("");
+    setSearchPerformed(false);
+    dispatch(clearSearch());
+    window.location.reload();
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleSearch();
+  };
+  const isSearchDisabled = name === "";
 
   return (
     <div className="flex z-20 fixed left-96 relative">
       <div className="bg-gray-800 py-2 px-6 flex items-center space-x-4 gap-5 rounded-md">
-        <input
-          className="py-1 px-2 rounded-md"
-          type="text"
-          placeholder="Especialidad..."
-          value={name}
-          style={{ width: "160px" }}
-          onChange={handleSearch}
-        />
-        {searchPerformed ? (
-          <button
-            className="bg-red-500 hover:bg-cimPallete-gold text-white font-bold py-1 px-2 rounded"
-            onClick={() => {
-              window.location.reload();
-            }}
-          >
-            Borrar
-          </button>
-        ) : (
-          <button
-            className="bg-cimPallete-600 hover:bg-cimPallete-gold text-white font-bold py-1 px-2 rounded"
-            onClick={(e) => fetchData(e)}
-          >
-            Buscar
-          </button>
-        )}
+        <form onSubmit={handleSubmit}>
+          {" "}
+          <input
+            className="py-1 px-2 rounded-md"
+            type="text"
+            placeholder="Especialidad..."
+            value={name}
+            style={{ width: "160px" }}
+            onChange={handleChange}
+          />
+          {searchPerformed ? (
+            <button
+              className="bg-red-500 hover:bg-cimPallete-gold text-white font-bold py-1 px-2 rounded"
+              onClick={handleReset}
+              type="submit"
+            >
+              Borrar
+            </button>
+          ) : (
+            <button
+              className="bg-cimPallete-600 hover:bg-cimPallete-gold text-white font-bold py-1 px-2 rounded"
+              onClick={handleSearch}
+              disabled={isSearchDisabled}
+              type="button"
+            >
+              Buscar
+            </button>
+          )}{" "}
+        </form>
+
         <div className="flex space-x-4">
           <h5 className="flex items-center self-center text-white">Ordenar:</h5>
           <button className="bg-cimPallete-600 hover:bg-cimPallete-gold text-white font-bold py-1 px-2 rounded">
