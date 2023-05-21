e0b4129c28188c12924c4b5b8280b5840b3b7c92
emilianoprioli <emilianoprioli.ep@gmail.com>
2023-05-21 17:51:10 -0300

    empty message

--- app/components/turnos/page.jsx
+++ app/components/turnos/page.jsx
@@ -1,119 +1,156 @@
-"use client";
-
-import { Alert, Calendar } from "antd";
-import "moment/locale/es";
-import locale from "antd/es/date-picker/locale/es_ES";
-import dayjs from "dayjs";
-import { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { getSpeciality } from "@/app/redux/reducer";
-import axios from "axios";
-// const backendURL = process.env.PUBLIC_BACKEND_URL;
-const backendURL = "https://medconnectback-production.up.railway.app";
-const specializationsURL = `${backendURL}/specializations`;
+"use client"
+import { Calendar, Alert, Col, Radio, Row, Select, Typography, theme } from 'antd';
+const dayjs = require('dayjs');
+const es = require('dayjs/locale/es'); 
+import dayLocaleData from 'dayjs/plugin/localeData';
+dayjs.extend(dayLocaleData);
+import { useEffect, useState } from 'react';
+import { useSelector,useDispatch } from 'react-redux';
+import { getSpeciality } from '@/app/redux/reducer';
+import axios from 'axios';
+import { getMedicos } from '@/app/redux/reducer';
+import {useEffect, useState } from 'react';
+import FormItem from 'antd/es/form/FormItem';
+import { useRouter } from 'next/navigation';
+import styles from './page.module.css'
 
 const Turnos = () => {
-  const today = dayjs();
-  const [value, setValue] = useState(today);
-  const [selectedValue, setSelectedValue] = useState(today);
-  const dispatch = useDispatch();
-  const { AllSpecial } = useSelector((state) => state.speciality);
-  const days = ["Mo", "Tu", "We", "Th", "Fr", "Sa"];
-  let check = false;
+  // let day = date["$d"].getDay()
+  dayjs.locale(es);
+  const today = dayjs()
+  const actualMonth = dayjs().month()
+  const [value, setValue] = useState();
+  const [selectedValue, setSelectedValue] = useState();
+    const dispatch = useDispatch()
+    const {AllSpecial} = useSelector(state => state.speciality)
+    let check = false
+    const { token } = theme.useToken();
+    const specialityDay = [];
 
-  //* this function select a random day of the week
-  const randomDay = () => {
-    const randomIndex = Math.round(Math.random() * days.length);
-    return days[randomIndex];
-  };
-
-  //* I temporarily add a new property to the response, setting it to a random day returned by the above function
-  const añadirDia = async () => {
-    const response = await axios.get(specializationsURL);
-    const test = response?.data.map((obj) => {
-      const selectedDay = randomDay();
-      return { ...obj, selectedDay };
-    });
-    dispatch(getSpeciality(test));
-  };
-
-  useEffect(() => {
-    if (!check) {
-      check = true;
-      añadirDia();
-    }
-  }, []);
-
-  const onSelect = (newValue) => {
-    setValue(newValue);
-    setSelectedValue(newValue);
-  };
-
-  const onPanelChange = (newValue) => {
-    setValue(newValue);
-  };
-
-  const disabledDate = (current) => {
-    // Deshabilitar las fechas anteriores al día de hoy
-    return current && current.isBefore(today.startOf("day"));
-  };
-
-  const auxTest = (value) => {
-    AllSpecial.map((obj) => {
-      console.log(obj);
-    });
-  };
-
-  const test = (date) => {
-    let day = date["$d"].getDay();
-    switch (day) {
-      case "Mo":
-        return auxTest();
-
-      case "Tu":
-        return auxTest();
-
-      case "We":
-        return auxTest();
-
-      case "Th":
-        return auxTest();
-
-      case "Fr":
-        return auxTest();
-
-      case "Sa":
-        return auxTest();
-      default:
-        break;
-    }
-  };
-
-  const dateFullCellRender = (date) => {
-    test(date);
-    console.log(date["$d"].getDay());
-    return <div>texto</div>;
-  };
-
-  return (
-    <>
-      <Alert
-        message={`Seleccionaste la fecha: ${selectedValue?.format(
-          "DD-MM-YYYY"
-        )}`}
-      />
-      <Calendar
-        format={"DD-MM-YYYY"}
-        locale={locale}
-        value={value}
-        fullscreen={true}
-        onSelect={onSelect}
-        onPanelChange={onPanelChange}
-        disabledDate={disabledDate}
-        cellRender={dateFullCellRender}
-      />
-    </>
-  );
+const randomDay = () => {
+  const randomIndex = Math.floor(Math.random() * 7);
+  return randomIndex;
 };
 
-export default Turnos;
+const añadirDia = async () => {
+  const response = await axios.get("http://localhost:3001/specializations");
+  const test = response?.data.map((obj) => {
+    const selectedDay = randomDay();
+    return { ...obj, selectedDay };
+  });
+  dispatch(getSpeciality(test));
+};
+
+const auxFunc = () => {
+  const updatedSpecialityDay = AllSpecial.map((obj) => {
+    return { [obj.selectedDay]: obj.name };
+  });
+  specialityDay.splice(0, specialityDay.length, ...updatedSpecialityDay);
+};
+
+useEffect(() => {
+  if (!AllSpecial.length) {
+    añadirDia();
+  }
+}, [AllSpecial]);
+
+useEffect(() => {
+  auxFunc();
+}, [AllSpecial]);
+
+console.log(specialityDay);
+
+          const onSelect = (newValue) => {
+            setValue(newValue);
+            setSelectedValue(newValue);
+          };
+          
+          const onPanelChange = (newValue) => {
+            setValue(newValue);
+            console.log("new value",newValue);
+            console.log(newValue.format('DD-MM-YYYY'));
+          };
+          
+          const disabledDate = async (current) => {
+          // Deshabilitar las fechas anteriores al día de hoy
+          if(specialityDay.length){
+            console.log(await specialityDay?.filter((obj) => obj))
+            if (!current.isBefore(today.startOf('day')) || !specialityDay.filter((obj) => Object.keys(obj).includes(current.day()))) {
+              return false;
+            }return true;
+          }
+        };
+  
+      
+
+        const wrapperStyle = {
+          width: 300,
+          border: `1px solid ${token.colorBorderSecondary}`,
+          borderRadius: token.borderRadiusLG,
+        };
+        
+        return (
+          <>
+        {/* <Alert message={`Seleccionaste la fecha: ${selectedValue?.format('DD-MM-YYYY')}`} /> */}
+        <div style={wrapperStyle}>
+      <Calendar
+        fullscreen={false}
+        headerRender={({ value, type, onChange, onTypeChange }) => {
+          const start = 0;
+          const end = 12;
+          const monthOptions = [];
+          let current = value.clone();
+          const localeData = value.localeData();
+          const months = [];
+          for (let i = 0; i < 12; i++) {
+            current = current.month(i).locale(es);
+            months.push(localeData.monthsShort(current));
+          }
+          for (let i = start; i < end; i++) {
+            if (i >= actualMonth) {
+              monthOptions.push(
+                <Select.Option key={i} value={i} className="month-item">
+                  {months[i]}
+                </Select.Option>,
+              );
+            }
+          }
+
+          const month = value.month();      
+          return (
+            <div
+              style={{
+                padding: 8,
+              }}
+            >
+              <Typography.Title level={4}>Custom header</Typography.Title>
+              <Row gutter={8}>
+                <Col>
+                  <p>Mes</p>
+                </Col>
+                <Col>
+                  <Select
+                    size="small"
+                    popupMatchSelectWidth={false}
+                    value={month}
+                    onChange={(newMonth) => {
+                      const now = value.clone().month(newMonth);
+                      onChange(now);
+                    }}
+                  >
+                    {monthOptions}
+                  </Select>
+                </Col>
+              </Row>
+            </div>
+          );
+        }}
+        disabledDate={disabledDate}
+        onChange={onPanelChange}
+      />
+    </div>
+      </>
+    );
+  };
+  
+  export default Turnos;
