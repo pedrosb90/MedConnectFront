611d314a71ba2cef5d78b77467e0f7ebe84f9c56
danielhp404 <daniel.404hrz@gmail.com>
2023-05-21 15:15:36 -0600

    bug fix

--- app/citas/Cards/CardSpecial.jsx
+++ app/citas/Cards/CardSpecial.jsx
@@ -7,11 +7,8 @@ import { getSpeciality } from "../../redux/reducer";
 import { useEffect, useState } from "react";
 import CardMedics from "./CardMedics";
 import Link from "next/link";
-<<<<<<< HEAD
 import {postInfo} from '../../redux/CitaReducer'
 
-=======
->>>>>>> develop
 // const backendURL = process.env.PUBLIC_BACKEND_URL;
 const local = "http://localhost:3001/specializations";
 const backendURL = "https://medconnectback-production.up.railway.app";
@@ -20,13 +17,9 @@ const specializationsURL = `${backendURL}/specializations`;
 export default function CardSpecial() {
   const dispatch = useDispatch();
   const especialidades = useSelector((state) => state.speciality.AllSpecial);
-<<<<<<< HEAD
   const [especial,setEspecial]= useState([])
   const[espeMed,setEspeMEd]=useState([])
   const [medico,setMedico]= useState({})
-=======
-  const [especial, setEspecial] = useState([]);
->>>>>>> develop
   async function fetchData() {
     try {
       const response = await axios.get(local);
@@ -38,7 +31,6 @@ export default function CardSpecial() {
   }
   const handleClick = (event) => {
     const nameES = event.target.name;
-<<<<<<< HEAD
     if(medico.id){
       const espeMed = medico.specializations.map(espe=>espe.name)
     
@@ -124,74 +116,6 @@ export default function CardSpecial() {
       
       
     </div>
-=======
-    if (nameES === "All") setEspecial(especialidades);
-    else setEspecial(especialidades.filter((espe) => espe.name === nameES));
-  };
-  const handleClickMed = (event) => {
-    const espeMed = event.specializations.map((espe) => espe.name);
-    const data = especialidades.filter((espe) => espeMed.includes(espe.name));
-    setEspecial(data);
-  };
-
-  useEffect(() => {
-    !especialidades.length ? fetchData() : setEspecial(especialidades);
-  }, [especialidades, fetchData]);
-
-  return (
-    <div>
-      <h2 className={styles.subTitle}>
-        ¿Quieres agendar con un profesional en particular?
-      </h2>
-      <CardMedics handleClickMed={handleClickMed}></CardMedics>
-
-      <h2 className={styles.subTitle}>
-        Selecciona los servicios que deseas agendar:
-      </h2>
-      <BottonEspe
-        especialidades={especialidades}
-        handleClick={handleClick}
-      ></BottonEspe>
-      <div className={styles.box_espe}>
-        {especialidades.length ? (
-          especial.map((espe) => {
-            return (
-              <div
-                key={espe.id}
-                className={`max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ${styles.cars_Espe}`}
-              >
-                <Link href={`/specializations/${espe.id}`}>
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    {espe.name}
-                  </h5>
-                </Link>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                  {espe.description}
-                </p>
-                <button className="inline-flex gap-2 items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                  Agregar servicio
-                  <svg
-                    className="h-5 w-5 text-white"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M12 4v16m8-8H4"
-                    />
-                  </svg>
-                </button>
-              </div>
-            );
-          })
-        ) : (
-          <h1>...Loading</h1>
-        )}
-      </div>
->>>>>>> develop
     </div>
   );
 }
