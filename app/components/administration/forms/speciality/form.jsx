0f8abb1909aa40158b9a3ca763d287ccb89c9076
drope <pstrauch@gmail.com>
2023-05-27 19:22:25 -0300

    fix bug duplicate

--- app/components/administration/forms/speciality/form.jsx
+++ app/components/administration/forms/speciality/form.jsx
@@ -99,182 +99,174 @@ export default function SpecialtyForm() {
     const apiSecret = "45snDqDmumENYPAmz0UET_PYGH4";
     const signature = generateSHA1(generateSinature(publicId, apiSecret));
 
-    const deleteImag = async () => {
-      const url = "https://api.cloudinary.com/v1_1/dipgqcdtq/image/destroy";
-      const timestamp = new Date().getTime();
-      const apiKey = "977319699313977";
-      const apiSecret = "45snDqDmumENYPAmz0UET_PYGH4";
-      const signature = generateSHA1(generateSinature(publicId, apiSecret));
-
-      try {
-        const response = await axios.post(url, {
-          public_id: publicId,
-          signature: signature,
-          api_key: apiKey,
-          timestamp: timestamp,
-        });
-        setImage({ array: [] });
-      } catch (error) {}
-    };
-
-    function imagePreview() {
-      if (loading === "true") {
-        return <h3>Cargando Imagenes...</h3>;
-      }
-      if (loading === "false") {
-        return (
-          <h3>
-            {image.array.length <= 0 ? (
-              <h1>No Hay Imagen</h1>
-            ) : (
-              image.array.map((items, index) => (
-                <img
-                  key={index}
-                  alt="Imagen"
-                  // className="h-16 w-28 pr-4 bg-cover "
-                  style={{
-                    width: "125px",
-                    height: "70px",
-                    backgroundSize: "cover",
-                    paddingRight: "15px",
-                  }}
-                  src={items}
-                ></img>
-              ))
-            )}
-          </h3>
-        );
-      }
-    }
-
-    const [form] = Form.useForm();
-    const FinishFailed = () => {
-      setError({ ...error, alert: false });
-    };
-    const successFunc = () => {
-      setSuccess({ ...success, alert: false });
-    };
-
-    return (
-      <div className={styles.container}>
-        <h1 className={styles.title}>Añadir Especialidad</h1>
-        <Form
-          labelCol={{ span: 6 }}
-          wrapperCol={{ span: 15 }}
-          layout="horizontal"
-          onFinish={(values) => onSubmit(values)}
-        >
-          <Form.Item
-            name="name"
-            label="Especialidad"
-            rules={[
-              { required: true, message: "Por favor ingrese una especialidad" },
-            ]}
-            hasFeedback
-          >
-            <Input name="name" placeholder="nombre..." />
-          </Form.Item>
-
-          <div className={styles.container}>
-            <Warning
-              alert={error.alert}
-              text={error.text}
-              FinishFailed={FinishFailed}
-            />
-            <Success
-              alert={success.alert}
-              text={success.text}
-              success={successFunc}
-            />
-            <h1 className={styles.title}>Añadir Especialidad</h1>
-            <Form
-              className={styles.form}
-              labelCol={{ span: 6 }}
-              wrapperCol={{ span: 15 }}
-              layout="horizontal"
-              form={form}
-              onFinish={(values) => {
-                onSubmit(values);
-                form.resetFields();
-                deleteImag();
-              }}
-            >
-              <Form.Item
-                name="name"
-                label="Especialidad"
-                rules={[
-                  {
-                    required: true,
-                    message: "Por favor ingrese una especialidad",
-                  },
-                ]}
-                hasFeedback
-              >
-                <Input name="name" placeholder="nombre..." />
-              </Form.Item>
-
-              <Container>
-                <Dropzone
-                  className="dropzone"
-                  onDrop={handleDrop}
-                  onChange={(e) => setImage(e.target.value)}
-                  value={image}
-                  rules={[
-                    { required: true, message: "Por favor ingrese una imagen" },
-                  ]}
-                >
-                  {({ getRootProps, getInputProps }) => (
-                    <section>
-                      <div {...getRootProps({ className: "dropzone" })}>
-                        <input {...getInputProps()} />
-                        <span className="cursor-pointer text-3xl">📂</span>
-                        <p className="cursor-pointer">
-                          Suelta tu imagen aqui, O da click para seleccionar
-                        </p>
-                      </div>
-                    </section>
-                  )}
-                </Dropzone>
-                <div className={styles.container_img}>
-                  <button
-                    className="active:outline-none text-white bg-red-700 hover:bg-red-800 active:ring-4 active:ring-red-300 font-medium rounded-lg text-sm px-2.5 py-2 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:active:ring-red-900"
-                    onClick={deleteImag}
-                  >
-                    Delete
-                  </button>
-                  {imagePreview()}
-                </div>
-              </Container>
-
-              <Form.Item
-                name="description"
-                label="Descripción"
-                rules={[
-                  {
-                    required: true,
-                    message: "Por favor ingrese una descripción",
-                  },
-                ]}
-                hasFeedback
-              >
-                <Input.TextArea
-                  name="description"
-                  placeholder="Descripción"
-                  style={{
-                    resize: "none",
-                    overflow: "hidden",
-                    paddingRight: "25px",
-                    height: "20px",
-                  }}
-                />
-              </Form.Item>
-              <Button htmlType="submit" className={styles.Button}>
-                Enviar
-              </Button>
-            </Form>
-          </div>
-        </Form>
-      </div>
-    );
+    try {
+      const response = await axios.post(url, {
+        public_id: publicId,
+        signature: signature,
+        api_key: apiKey,
+        timestamp: timestamp,
+      });
+      setImage({ array: [] });
+    } catch (error) {}
   };
+
+  function imagePreview() {
+    if (loading === "true") {
+      return <h3>Cargando Imagenes...</h3>;
+    }
+    if (loading === "false") {
+      return (
+        <h3>
+          {image.array.length <= 0 ? (
+            <h1>No Hay Imagen</h1>
+          ) : (
+            image.array.map((items, index) => (
+              <img
+                key={index}
+                alt="Imagen"
+                // className="h-16 w-28 pr-4 bg-cover "
+                style={{
+                  width: "125px",
+                  height: "70px",
+                  backgroundSize: "cover",
+                  paddingRight: "15px",
+                }}
+                src={items}
+              ></img>
+            ))
+          )}
+        </h3>
+      );
+    }
+  }
+
+  const [form] = Form.useForm();
+  const FinishFailed = () => {
+    setError({ ...error, alert: false });
+  };
+  const successFunc = () => {
+    setSuccess({ ...success, alert: false });
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Añadir Especialidad</h1>
+      <Form
+        labelCol={{ span: 6 }}
+        wrapperCol={{ span: 15 }}
+        layout="horizontal"
+        onFinish={(values) => onSubmit(values)}
+      >
+        <Form.Item
+          name="name"
+          label="Especialidad"
+          rules={[
+            { required: true, message: "Por favor ingrese una especialidad" },
+          ]}
+          hasFeedback
+        >
+          <Input name="name" placeholder="nombre..." />
+        </Form.Item>
+
+        <div className={styles.container}>
+          <Warning
+            alert={error.alert}
+            text={error.text}
+            FinishFailed={FinishFailed}
+          />
+          <Success
+            alert={success.alert}
+            text={success.text}
+            success={successFunc}
+          />
+          <h1 className={styles.title}>Añadir Especialidad</h1>
+          <Form
+            className={styles.form}
+            labelCol={{ span: 6 }}
+            wrapperCol={{ span: 15 }}
+            layout="horizontal"
+            form={form}
+            onFinish={(values) => {
+              onSubmit(values);
+              form.resetFields();
+              deleteImag();
+            }}
+          >
+            <Form.Item
+              name="name"
+              label="Especialidad"
+              rules={[
+                {
+                  required: true,
+                  message: "Por favor ingrese una especialidad",
+                },
+              ]}
+              hasFeedback
+            >
+              <Input name="name" placeholder="nombre..." />
+            </Form.Item>
+
+            <Container>
+              <Dropzone
+                className="dropzone"
+                onDrop={handleDrop}
+                onChange={(e) => setImage(e.target.value)}
+                value={image}
+                rules={[
+                  { required: true, message: "Por favor ingrese una imagen" },
+                ]}
+              >
+                {({ getRootProps, getInputProps }) => (
+                  <section>
+                    <div {...getRootProps({ className: "dropzone" })}>
+                      <input {...getInputProps()} />
+                      <span className="cursor-pointer text-3xl">📂</span>
+                      <p className="cursor-pointer">
+                        Suelta tu imagen aqui, O da click para seleccionar
+                      </p>
+                    </div>
+                  </section>
+                )}
+              </Dropzone>
+              <div className={styles.container_img}>
+                <button
+                  className="active:outline-none text-white bg-red-700 hover:bg-red-800 active:ring-4 active:ring-red-300 font-medium rounded-lg text-sm px-2.5 py-2 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:active:ring-red-900"
+                  onClick={deleteImag}
+                >
+                  Delete
+                </button>
+                {imagePreview()}
+              </div>
+            </Container>
+
+            <Form.Item
+              name="description"
+              label="Descripción"
+              rules={[
+                {
+                  required: true,
+                  message: "Por favor ingrese una descripción",
+                },
+              ]}
+              hasFeedback
+            >
+              <Input.TextArea
+                name="description"
+                placeholder="Descripción"
+                style={{
+                  resize: "none",
+                  overflow: "hidden",
+                  paddingRight: "25px",
+                  height: "20px",
+                }}
+              />
+            </Form.Item>
+            <Button htmlType="submit" className={styles.Button}>
+              Enviar
+            </Button>
+          </Form>
+        </div>
+      </Form>
+    </div>
+  );
 }
