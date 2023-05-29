b959a7ee865e25ee404f30c364602d2d8dd76909
drope <pstrauch@gmail.com>
2023-05-28 21:18:21 -0300

    pic format

--- app/page.jsx
+++ app/page.jsx
@@ -104,8 +104,8 @@ export default function Home() {
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
-            height: "100%",
-            width: "100%",
+            height: "60%",
+            width: "75%",
           }}
         />
         <div className="text-center ">
