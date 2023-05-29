195ab476ec0b88a07b659437417b657f75f77691
drope <pstrauch@gmail.com>
2023-05-28 22:36:53 -0300

    button pos

--- app/medicos/[id]/page.jsx
+++ app/medicos/[id]/page.jsx
@@ -138,8 +138,9 @@ export default function Page() {
           className={`text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 `}
           style={{
             height: "40px",
+            width: "90",
             position: "absolute",
-            bottom: "5%",
+            bottom: "9%",
             right: "80%",
           }}
         >
@@ -154,7 +155,8 @@ export default function Page() {
             height: "40px",
             position: "absolute",
             bottom: "5%",
-            right: "40%",
+            right: "15%",
+            width: "90",
           }}
         >
           Citas
