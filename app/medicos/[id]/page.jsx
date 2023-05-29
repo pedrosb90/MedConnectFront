c956e0d25525e09a5e7a109083212b31d5be2f44
drope <pstrauch@gmail.com>
2023-05-28 22:14:45 -0300

    styles/remove unwanted

--- app/medicos/[id]/page.jsx
+++ app/medicos/[id]/page.jsx
@@ -144,7 +144,6 @@ export default function Page() {
           <button
             type="button"
             className={`text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 ${styles.btn_return_home}`}
-            style={{ height: "40px", marginRight: "6px" }}
           >
             Inicio
           </button>
@@ -153,7 +152,6 @@ export default function Page() {
           <button
             type="button"
             className={`text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm dark:bg-gray-600 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 ${styles.btn_return_citas}`}
-            style={{ height: "40px" }}
           >
             Citas
           </button>
