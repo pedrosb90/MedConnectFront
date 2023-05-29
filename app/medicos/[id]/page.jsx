56d3c11bbca1afa4d029399cc286381b399060e0
drope <pstrauch@gmail.com>
2023-05-28 22:34:15 -0300

    zzz

--- app/medicos/[id]/page.jsx
+++ app/medicos/[id]/page.jsx
@@ -131,46 +131,35 @@ export default function Page() {
           </div>
         </div>
       </section>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "flex-end",
-          position: "absolute",
-          bottom: "5%",
-          right: "16%",
-        }}
-      >
-        <div style={{ position: "relative" }}>
-          <Link href="/">
-            <button
-              type="button"
-              className={`text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 `}
-              style={{
-                height: "40px",
-                position: "absolute",
-                bottom: "5%",
-                right: "80%",
-              }}
-            >
-              Inicio
-            </button>
-          </Link>
-          <Link href="/citas">
-            <button
-              type="button"
-              className={`text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm dark:bg-gray-600 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 `}
-              style={{
-                height: "40px",
-                position: "absolute",
-                bottom: "5%",
-                right: "40%",
-              }}
-            >
-              Citas
-            </button>
-          </Link>
-        </div>
-      </div>
+
+      <Link href="/">
+        <button
+          type="button"
+          className={`text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 `}
+          style={{
+            height: "40px",
+            position: "absolute",
+            bottom: "5%",
+            right: "80%",
+          }}
+        >
+          Inicio
+        </button>
+      </Link>
+      <Link href="/citas">
+        <button
+          type="button"
+          className={`text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm dark:bg-gray-600 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 `}
+          style={{
+            height: "40px",
+            position: "absolute",
+            bottom: "5%",
+            right: "40%",
+          }}
+        >
+          Citas
+        </button>
+      </Link>
     </div>
   );
 }
