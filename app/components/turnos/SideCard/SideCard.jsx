948595e9a29507b4e7ef6cf17917c0479a4cc45d
drope <pstrauch@gmail.com>
2023-05-30 15:48:36 -0300

    resumendecompra/ maqueta en desarrollo

--- app/components/turnos/SideCard/SideCard.jsx
+++ app/components/turnos/SideCard/SideCard.jsx
@@ -2,9 +2,17 @@ import { useSelector, useDispatch } from "react-redux";
 import styles from "../../turnos/CardEdit.module.css";
 
 const SideCard = () => {
+  const { info, citas } = useSelector((state) => state.cita);
+
   return (
     <div className={styles.resumen}>
-      <h1 className={styles.resumenText}>Resumen de orden</h1>
+      <h1 className={styles.resumenTitle}>Resumen de orden</h1>
+      <br />
+      <div className={styles.resumenText}>
+        <p>Especialidad: {info.especialidad}</p>
+        {"render info"}
+        <p className={styles.costo}>Costo final: </p>
+      </div>
     </div>
   );
 };
