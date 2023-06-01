641f4f51e322c9d7efeb018ec33cfdc1f7487b79
emilianoprioli <emilianoprioli.ep@gmail.com>
2023-06-01 03:51:30 -0300

    changes in file for testing

--- app/citas/page.jsx
+++ app/citas/page.jsx
@@ -13,6 +13,7 @@ export default function Citas() {
   const { login } = useSelector((state) => state);
 
 
+  
   useEffect(() => {
     (!login.userGoogle || !login.userLocal) && nav.push("/components/forms/UserLogin");
   }, []);
