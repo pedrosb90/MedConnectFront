40ceba825c330c741e2efb67f817e2737a03fa31
sebasmayor06 <sebda06@gmail.com>
2023-05-31 23:03:54 -0500

    arreglo EditPaciente

--- app/components/administration/records/pacientes/page.jsx
+++ app/components/administration/records/pacientes/page.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import styles from "./page.module.css";
 import Warning from "../../../warning/Warning";
 import Success from "../../../success/Success";
-import EditPaciente from "./editPaciente";
+import EditPaciente from "./EditPaciente";
 import { useSelector } from "react-redux";
 export default function Pacientes() {
   const [pacientes, setPacientes] = useState([]);
